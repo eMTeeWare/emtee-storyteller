48a03803a980c5a53d6994db304f7c84ff49fac3
redcrafter502 <oskar.bahr.lg@gmail.com>
2021-03-29 08:14:42 +0200

    Add JavaFX to the project

--- src/main/kotlin/net/emteeware/Hello.kt
+++ src/main/kotlin/net/emteeware/Hello.kt
@@ -1,6 +1,26 @@
 package net.emteeware
 
-fun main(args: Array<String>) {
-    println("Hello, World")
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+
+class Hello: Application() {
+    override fun start(stage: Stage) {
+        val label = Label("eMTee Storyteller")
+        val scene = Scene(StackPane(label), 640.0, 480.0)
+        stage.scene = scene
+        stage.title = "eMTee Storyteller"
+        stage.show()
+    }
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch(Hello::class.java)
+        }
+    }
+
 }
 
